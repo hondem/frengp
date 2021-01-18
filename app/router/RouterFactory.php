7226ca04c369f450be38eb45dcd6560226d98379
riki123456 <miroslav.petras@lumitrix.eu>
2021-01-18 18:54:51 +0100

    add https

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -1,32 +1,1 @@
-<?php
-
-namespace App;
-
-use Nette;
-use Nette\Application\Routers\RouteList;
-use Nette\Application\Routers\Route;
-
-
-class RouterFactory
-{
-
-	/**
-	 * @return Nette\Application\IRouter
-	 */
-	public static function createRouter()
-	{
-		$router = new RouteList;
-                $router[] = new Route('admin/', 'Admin:Homepage:default');
-                $router[] = new Route('novinka/<newsId>', 'Homepage:showNews');
-                $router[] = new Route('stranka/<pageId>', 'Pages:show');
-                $router[] = new Route('podstranka/<subpageId>', 'SubPages:show');
-                $router[] = new Route('galerie/<galleryId>', 'Gallery:showGallery');
-                $router[] = new Route('galerie/slozka/<folderId>', 'Gallery:showFolder');
-                $router[] = new Route('rubrika/<sectionId>', 'Sections:show');
-                $router[] = new Route('mapa/', 'SiteStructure:default');
-                
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
-		return $router;
-	}
-
-}
+<?phpnamespace App;use Nette;use Nette\Application\Routers\RouteList;use Nette\Application\Routers\Route;class RouterFactory{	/**	 * @return Nette\Application\IRouter	 */	public static function createRouter()	{        Route::$defaultFlags = Route::SECURED;		$router = new RouteList;                $router[] = new Route('admin/', 'Admin:Homepage:default');                $router[] = new Route('novinka/<newsId>', 'Homepage:showNews');                $router[] = new Route('stranka/<pageId>', 'Pages:show');                $router[] = new Route('podstranka/<subpageId>', 'SubPages:show');                $router[] = new Route('galerie/<galleryId>', 'Gallery:showGallery');                $router[] = new Route('galerie/slozka/<folderId>', 'Gallery:showFolder');                $router[] = new Route('rubrika/<sectionId>', 'Sections:show');                $router[] = new Route('mapa/', 'SiteStructure:default');                		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');		return $router;	}}
